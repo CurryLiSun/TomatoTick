55c997c34a64e6b62328d5cb7a20ab8e5f49f78e
currysun <justdomyselfli@gmail.com>
2019-03-04 22:28:22 +0800

    no run setInterval

--- src/MyTimer.js
+++ src/MyTimer.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import './MyTimer.css';
 
 class MyTimer extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
-            value: '',
-            ss: ''
+            value: 0
         };
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.activateTimer = this.activateTimer.bind(this);
+        this.resetTimer = this.resetTimer.bind(this);
     }
 
     handleChange(event) {
@@ -23,8 +25,16 @@ class MyTimer extends Component {
     }
 
     activateTimer(event){
-        console.log("be click!");
+        //預想是將輸入數值更新至state的value
         //this.setState({value: event.target.value});
+        console.log(this);
+        console.log(this.setInterval);
+        //this.setInterval = setInterval(this.value.bind(this),1000)
+    }
+
+    resetTimer(event){
+        //console.log(event);
+        this.setState({value: 0});
     }
 
     render() {
@@ -35,11 +45,12 @@ class MyTimer extends Component {
             <br></br>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
             <br></br>
-            {/* <input type="text" value={this.state.value} onChange={this.handleChange} /> */}
             <button onClick={this.activateTimer}>
-            開始
+                開始
             </button>
-            <button>重置</button>
+            <button onClick={this.resetTimer}>
+                重置
+            </button>
         </div>
         );
     }
