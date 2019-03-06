99a6578239f1f7562134319fe6db4d967059eec5
currysun <justdomyselfli@gmail.com>
2019-03-06 22:15:29 +0800

    clock born

--- src/MyTimer.js
+++ src/MyTimer.js
@@ -6,13 +6,37 @@ class MyTimer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: 0
+            value: 0,
+            date: new Date()
         };
+        // 指定this
+        this.tick = this.tick.bind(this);
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.activateTimer = this.activateTimer.bind(this);
         this.resetTimer = this.resetTimer.bind(this);
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount");
+        this.timerID = setInterval(
+            ()=>this.tick(),
+            1000
+        );
+    }
+    
+    componentWillUnmount() {
+        console.log("componentWillUnmount");
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        let temp = this.state.value-1;
+        this.setState({
+            date: new Date(),
+            value:temp
+        });
     }
 
     handleChange(event) {
@@ -41,6 +65,8 @@ class MyTimer extends Component {
         return (
         <div className="MyTimer">
             <h1>My Timer</h1>
+            <h1>Hello, world!</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             <label>{this.state.value}</label>
             <br></br>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
