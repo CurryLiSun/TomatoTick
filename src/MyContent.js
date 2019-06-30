112bb8e5f5c96bda8e6c5db0a9f5d1f651e6a2f5
currysun <justdomyselfli@gmail.com>
2019-06-30 23:02:43 +0800

    update resume

--- src/MyContent.js
+++ src/MyContent.js
@@ -80,13 +80,16 @@ class MyContent extends Component {
             <div class="container-fluid p-0">
                 <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
                     <div class="w-100">
-                        <h1 class="mb-0">Curry
-                        <span class="text-primary">Sun</span>
-                        </h1>
-                        <div class="subheading mb-5">3542 Berry Street · Cheyenne Wells, CO 80810 · 0984-341-635 ·
-                        <a href="#">name@email.com</a>
+                        <h2 class="mb-0">孫
+                        <span class="text-primary">揚理</span>
+                        </h2>
+                        <h4 class="mb-0">
+                        <span class="text-primary">curry</span>
+                        </h4>
+                        <div class="subheading mb-5">802高雄市苓雅區福德三路317巷21號 · 0984-341-635 ·
+                        <a href="#">justdomyselfli@gmail.com</a>
                         </div>
-                        <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
+                        <p class="lead mb-5">軟體工程師</p>
                         <div class="social-icons">
                         <a href="#">
                             <i class="fab fa-linkedin-in"></i>
@@ -112,45 +115,45 @@ class MyContent extends Component {
 
                         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="resume-content">
-                            <h3 class="mb-0">Senior Web Developer</h3>
-                            <div class="subheading mb-3">Intelitec Solutions</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                            <h3 class="mb-0">Web軟體工程</h3>
+                            <div class="subheading mb-3">軟體工程師</div>
+                            <p>以Web Form架構開發，開發業務所需之網站</p>
                         </div>
                         <div class="resume-date text-md-right">
-                            <span class="text-primary">March 2013 - Present</span>
+                            <span class="text-primary">2018 - 2019</span>
                         </div>
                         </div>
 
                         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="resume-content">
-                            <h3 class="mb-0">Web Developer</h3>
-                            <div class="subheading mb-3">Intelitec Solutions</div>
-                            <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+                            <h3 class="mb-0">Web Service軟體工程</h3>
+                            <div class="subheading mb-3">實習工程師</div>
+                            <p>以Ext.js開發後台介面，並且以PHP撰寫後端Web Service功能</p>
                         </div>
                         <div class="resume-date text-md-right">
-                            <span class="text-primary">December 2011 - March 2013</span>
+                            <span class="text-primary">2013 - 2015</span>
                         </div>
                         </div>
 
                         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="resume-content">
-                            <h3 class="mb-0">Junior Web Designer</h3>
-                            <div class="subheading mb-3">Shout! Media Productions</div>
-                            <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                            <h3 class="mb-0">Web專題開發</h3>
+                            <div class="subheading mb-3">軟體工程師</div>
+                            <p>以HTML,CSS,JavaScript,JQuery架設專題網站，並且以PHP撰寫後端Web Service雲端推播功能</p>
                         </div>
                         <div class="resume-date text-md-right">
-                            <span class="text-primary">July 2010 - December 2011</span>
+                            <span class="text-primary">2012 - 2013</span>
                         </div>
                         </div>
 
                         <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
                         <div class="resume-content">
-                            <h3 class="mb-0">Web Design Intern</h3>
-                            <div class="subheading mb-3">Shout! Media Productions</div>
-                            <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                            <h3 class="mb-0"></h3>
+                            <div class="subheading mb-3"></div>
+                            <p></p>
                         </div>
                         <div class="resume-date text-md-right">
-                            <span class="text-primary">September 2008 - June 2010</span>
+                            <span class="text-primary"></span>
                         </div>
                         </div>
 
@@ -166,24 +169,25 @@ class MyContent extends Component {
 
                         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="resume-content">
-                            <h3 class="mb-0">University of Colorado Boulder</h3>
-                            <div class="subheading mb-3">Bachelor of Science</div>
-                            <div>Computer Science - Web Development Track</div>
-                            <p>GPA: 3.23</p>
+                            <h3 class="mb-0">樹德科技大學</h3>
+                            <div class="subheading mb-3">碩士</div>
+                            <div>資訊工程所</div>
+                            <p></p>
                         </div>
                         <div class="resume-date text-md-right">
-                            <span class="text-primary">August 2006 - May 2010</span>
+                            <span class="text-primary">2015 - 2017</span>
                         </div>
                         </div>
 
                         <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
                         <div class="resume-content">
-                            <h3 class="mb-0">James Buchanan High School</h3>
-                            <div class="subheading mb-3">Technology Magnet Program</div>
+                            <h3 class="mb-0">樹德科技大學</h3>
+                            <div class="subheading mb-3">學士</div>
+                            <div>資訊工程系</div>
                             <p>GPA: 3.56</p>
                         </div>
                         <div class="resume-date text-md-right">
-                            <span class="text-primary">August 2002 - May 2006</span>
+                            <span class="text-primary">2011 - 2015</span>
                         </div>
                         </div>
 
@@ -199,25 +203,25 @@ class MyContent extends Component {
                         <div class="subheading mb-3">Programming Languages &amp; Tools</div>
                         <ul class="list-inline dev-icons">
                         <li class="list-inline-item">
-                            <i class="fab fa-html5"></i>
+                            <i class="fab fa-html5">PHP</i>
                         </li>
                         <li class="list-inline-item">
-                            <i class="fab fa-css3-alt"></i>
+                            <i class="fab fa-css3-alt">JavaScript</i>
                         </li>
                         <li class="list-inline-item">
-                            <i class="fab fa-js-square"></i>
+                            <i class="fab fa-js-square">VS Code</i>
                         </li>
                         <li class="list-inline-item">
-                            <i class="fab fa-angular"></i>
+                            <i class="fab fa-angular">CSS</i>
                         </li>
                         <li class="list-inline-item">
-                            <i class="fab fa-react"></i>
+                            <i class="fab fa-react">React.js</i>
                         </li>
                         <li class="list-inline-item">
-                            <i class="fab fa-node-js"></i>
+                            <i class="fab fa-node-js">Git</i>
                         </li>
                         <li class="list-inline-item">
-                            <i class="fab fa-sass"></i>
+                            <i class="fab fa-sass">Wordpress</i>
                         </li>
                         <li class="list-inline-item">
                             <i class="fab fa-less"></i>
@@ -240,16 +244,20 @@ class MyContent extends Component {
                         <ul class="fa-ul mb-0">
                         <li>
                             <i class="fa-li fa fa-check"></i>
-                            Mobile-First, Responsive Design</li>
+                            規劃/需求分析
+                            </li>
                         <li>
                             <i class="fa-li fa fa-check"></i>
-                            Cross Browser Testing &amp; Debugging</li>
+                            畫面布局設計
+                            </li>
                         <li>
                             <i class="fa-li fa fa-check"></i>
-                            Cross Functional Teams</li>
+                            撰寫邏輯流程
+                            </li>
                         <li>
                             <i class="fa-li fa fa-check"></i>
-                            Agile Development &amp; Scrum</li>
+                            測試/Debug
+                            </li>
                         </ul>
                     </div>
                 </section>
@@ -259,8 +267,11 @@ class MyContent extends Component {
                 <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
                     <div class="w-100">
                         <h2 class="mb-5">Interests</h2>
-                        <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                        <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                        <p>寫程式</p>
+                        <p>魔術</p>
+                        <p>桌遊</p>
+                        <p>跳舞</p>
+                        <p class="mb-0"></p>
                     </div>
                 </section>
 
@@ -272,30 +283,16 @@ class MyContent extends Component {
                         <ul class="fa-ul mb-0">
                         <li>
                             <i class="fa-li fa fa-trophy text-warning"></i>
-                            Google Analytics Certified Developer</li>
+                            優<sup>等</sup>
+                            2014院專題展</li>
                         <li>
                             <i class="fa-li fa fa-trophy text-warning"></i>
-                            Mobile Web Specialist - Google Certification</li>
-                        <li>
-                            <i class="fa-li fa fa-trophy text-warning"></i>
-                            1<sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009</li>
-                        <li>
-                            <i class="fa-li fa fa-trophy text-warning"></i>
-                            1<sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)</li>
+                            優<sup>等</sup>
+                            2013行動端創新應用APP</li>
                         <li>
                             <i class="fa-li fa fa-trophy text-warning"></i>
                             2<sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008</li>
-                        <li>
-                            <i class="fa-li fa fa-trophy text-warning"></i>
-                            1<sup>st</sup>
-                            Place - James Buchanan High School - Hackathon 2006</li>
-                        <li>
-                            <i class="fa-li fa fa-trophy text-warning"></i>
-                            3<sup>rd</sup>
-                            Place - James Buchanan High School - Hackathon 2005</li>
+                            2012行動應用創作軟體競賽</li>
                         </ul>
                     </div>
                 </section>
